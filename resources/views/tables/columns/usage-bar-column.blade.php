2ae60d9876aa45742fada158d7add78155c61100
Marin Delija <marindelija@gmail.com>
2025-01-07 13:26:36 +0100

    ..

--- resources/views/tables/columns/usage-bar-column.blade.php
+++ resources/views/tables/columns/usage-bar-column.blade.php
@@ -1,10 +1,25 @@
 <div class="flex items-center w-full gap-2">
     @php
         $state = $getState();
+        $value = $state['value'] ?? 0;
+        $color = match(true) {
+            $value >= 80 => 'bg-danger-500',
+            $value >= 70 => 'bg-warning-500',
+            default => 'bg-success-500'
+        };
     @endphp
 
-    {{-- Debug output --}}
-    <div>
-        Raw state: {{ var_export($state, true) }}
+    <div class="flex-1">
+        <div class="bg-gray-200 dark:bg-gray-700 rounded-lg overflow-hidden h-2">
+            <div 
+                class="{{ $color }} h-2 transition-all" 
+                style="width: {{ $value }}%"
+                title="{{ $state['tooltip'] ?? '' }}"
+            ></div>
+        </div>
+    </div>
+
+    <div class="min-w-[3rem] text-sm text-end tabular-nums">
+        {{ number_format($value, 1) }}%
     </div>
 </div> 
