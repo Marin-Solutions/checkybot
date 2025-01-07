637c3258241bb827bc8bdfcadb0bc8af8807865a
Marin Delija <marindelija@gmail.com>
2025-01-07 14:49:10 +0100

    ..

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    {{-- ... other head elements ... --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/filament.css'])
+</head>
+<body>
+    {{-- ... --}}
+</body>
+</html> 
