5564fe05c5f5391ad95c89050471da61f1c2f01c
Marin Delija <marindelija@gmail.com>
2025-01-12 20:21:33 +0100

    ..

--- app/Filament/Resources/NotificationSettingResource.php
+++ app/Filament/Resources/NotificationSettingResource.php
@@ -1,129 +1,110 @@
 <?php
 
-    namespace App\Filament\Resources;
+namespace App\Filament\Resources;
 
-    use App\Enums\NotificationChannelTypesEnum;
-    use App\Enums\NotificationScopesEnum;
-    use App\Enums\WebsiteServicesEnum;
-    use App\Filament\Resources\NotificationSettingResource\Pages;
-    use App\Filament\Resources\NotificationSettingResource\RelationManagers;
-    use App\Models\NotificationSetting;
-    use Filament\Forms;
-    use Filament\Forms\Form;
-    use Filament\Resources\Resource;
-    use Filament\Tables;
-    use Filament\Tables\Table;
-    use Illuminate\Database\Eloquent\Builder;
-    use Illuminate\Database\Eloquent\SoftDeletingScope;
-    use Illuminate\Support\Facades\Auth;
-    use Illuminate\Support\HtmlString;
+use App\Enums\NotificationChannelTypesEnum;
+use App\Enums\WebsiteServicesEnum;
+use App\Filament\Resources\NotificationSettingResource\Pages;
+use App\Models\NotificationSetting;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
 
-    class NotificationSettingResource extends Resource
+class NotificationSettingResource extends Resource
+{
+    protected static ?string $model = NotificationSetting::class;
+    protected static ?string $navigationIcon = 'heroicon-o-cog';
+    protected static ?string $navigationGroup = 'Operations';
+    protected static ?int $navigationSort = 5;
+    protected static ?string $modelLabel = 'Global Notification';
+    protected static ?string $pluralModelLabel = 'Global Notifications';
+
+    public static function getNavigationBadge(): ?string
     {
-        protected static ?string $model = NotificationSetting::class;
-
-        protected static ?string $navigationIcon = 'heroicon-o-cog';
-        protected static ?string $navigationGroup = 'Operations';
-        protected static ?int $navigationSort = 5;
-
-        /**
-         * Get the navigation badge for the resource.
-         */
-        public static function getNavigationBadge(): ?string
-        {
-            return number_format(static::getModel()::count());
-        }
-
-        public static function form( Form $form ): Form
-        {
-            return $form
-                ->schema([
-                    Forms\Components\Fieldset::make('Notification Setting')
-                        ->schema([
-                            Forms\Components\Select::make('scope')
-                                ->options(NotificationScopesEnum::toArray())
-                                ->required()
-                                ->live()
-                                ->columnSpan(1),
-                            Forms\Components\Select::make('website_id')
-                                ->relationship(name: 'website', titleAttribute: 'name')
-                                ->reactive()
-                                ->required(fn( $get ) => $get("scope") === NotificationScopesEnum::WEBSITE->name)
-                                ->disabled(fn( $get ) => $get("scope") !== NotificationScopesEnum::WEBSITE->name)
-                                ->columnSpan(1),
-                            Forms\Components\Select::make('inspection')
-                                ->options(WebsiteServicesEnum::toArray())
-                                ->required()
-                                ->columnSpan(1),
-                        ])->columns(3),
-                    Forms\Components\Fieldset::make('Notification Channel')
-                        ->schema([
-                            Forms\Components\Select::make('channel_type')
-                                ->options(NotificationChannelTypesEnum::toArray())
-                                ->required()
-                                ->reactive()
-                                ->columnSpan(1),
-                            Forms\Components\TextInput::make('address')
-                                ->label("Email")
-                                ->required()
-                                ->columnSpan(1)
-                                ->rules(function ( callable $get ) {
-                                    return match ( $get('channel_type') ) {
-                                        NotificationChannelTypesEnum::MAIL->name => [ 'required', 'email' ],
-                                        NotificationChannelTypesEnum::WEBHOOK->name => [ 'required', 'url' ],
-                                    };
-                                })
-                                ->hidden(fn( $get ) => $get("channel_type") !== NotificationChannelTypesEnum::MAIL->name),
-                            Forms\Components\Select::make('notification_channel_id')
-                                ->label('Notification Channel')
-                                ->required()
-                                ->options(\auth()->user()->webhookChannels()->pluck('title', 'id'))
-                                ->hidden(fn( callable $get ) => $get("channel_type") !== NotificationChannelTypesEnum::WEBHOOK->name)
-                        ])->columns(2),
-                ])
-            ;
-        }
-
-        public static function table( Table $table ): Table
-        {
-            return $table
-                ->columns([
-                    Tables\Columns\TextColumn::make('scope_value'),
-                    Tables\Columns\TextColumn::make('inspection_value'),
-                    Tables\Columns\TextColumn::make('channel_type_value'),
-                    Tables\Columns\TextColumn::make('website.name')->toggleable(isToggledHiddenByDefault: true),
-                    Tables\Columns\TextColumn::make('user.name')->toggleable(isToggledHiddenByDefault: true),
-                    Tables\Columns\TextColumn::make('channel.title')->label('Channel Name')->toggleable(isToggledHiddenByDefault: true),
-                    Tables\Columns\TextColumn::make('address'),
-                    Tables\Columns\ToggleColumn::make('flag_active')
-                ])
-                ->filters([
-                    //
-                ])
-                ->actions([
-                    Tables\Actions\EditAction::make(),
-                ])
-                ->bulkActions([
-                    Tables\Actions\BulkActionGroup::make([
-                        Tables\Actions\DeleteBulkAction::make(),
-                    ]),
-                ])
-            ;
-        }
-
-        public static function getRelations(): array
-        {
-            return [
-                //
-            ];
-        }
-
-        public static function getPages(): array
-        {
-            return [
-                'index'  => Pages\ListNotificationSettings::route('/'),
-                'create' => Pages\CreateNotificationSetting::route('/create'),
-                'edit'   => Pages\EditNotificationSetting::route('/{record}/edit'),
-            ];
-        }
+        return number_format(static::getModel()::where('user_id', auth()->id())->count());
     }
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Fieldset::make('Notification Setting')
+                    ->schema([
+                        Forms\Components\Select::make('inspection')
+                            ->label('Monitor')
+                            ->options(WebsiteServicesEnum::toArray())
+                            ->required(),
+                    ])->columns(1),
+                Forms\Components\Fieldset::make('Notification Channel')
+                    ->schema([
+                        Forms\Components\Select::make('channel_type')
+                            ->options(NotificationChannelTypesEnum::toArray())
+                            ->required()
+                            ->reactive()
+                            ->columnSpan(1),
+                        Forms\Components\TextInput::make('address')
+                            ->label("Email")
+                            ->required()
+                            ->columnSpan(1)
+                            ->rules(function (callable $get) {
+                                return match ($get('channel_type')) {
+                                    NotificationChannelTypesEnum::MAIL->name => ['required', 'email'],
+                                    NotificationChannelTypesEnum::WEBHOOK->name => ['required', 'url'],
+                                };
+                            })
+                            ->hidden(fn($get) => $get("channel_type") !== NotificationChannelTypesEnum::MAIL->name),
+                        Forms\Components\Select::make('notification_channel_id')
+                            ->label('Notification Channel')
+                            ->required()
+                            ->options(fn() => auth()->user()->webhookChannels()->pluck('title', 'id'))
+                            ->hidden(fn($get) => $get("channel_type") !== NotificationChannelTypesEnum::WEBHOOK->name)
+                    ])->columns(2),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('inspection_value')
+                    ->label('Monitor'),
+                Tables\Columns\TextColumn::make('channel_type_value')
+                    ->label('Channel Type'),
+                Tables\Columns\TextColumn::make('channel.title')
+                    ->label('Channel Name')
+                    ->visible(fn($record) => $record->channel_type === NotificationChannelTypesEnum::WEBHOOK->name),
+                Tables\Columns\TextColumn::make('address')
+                    ->visible(fn($record) => $record->channel_type === NotificationChannelTypesEnum::MAIL->name),
+                Tables\Columns\ToggleColumn::make('flag_active')
+                    ->label('Active'),
+            ])
+            ->filters([])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->where('user_id', auth()->id());
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListNotificationSettings::route('/'),
+            'create' => Pages\CreateNotificationSetting::route('/create'),
+            'edit' => Pages\EditNotificationSetting::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Filament/Resources/NotificationSettingResource/Pages/CreateNotificationSetting.php
+++ app/Filament/Resources/NotificationSettingResource/Pages/CreateNotificationSetting.php
@@ -1,37 +1,36 @@
 <?php
 
-    namespace App\Filament\Resources\NotificationSettingResource\Pages;
+namespace App\Filament\Resources\NotificationSettingResource\Pages;
 
-    use App\Enums\NotificationChannelTypesEnum;
-    use App\Filament\Resources\NotificationSettingResource;
-    use Filament\Actions;
-    use Filament\Resources\Pages\CreateRecord;
+use App\Enums\NotificationChannelTypesEnum;
+use App\Enums\NotificationScopesEnum;
+use App\Filament\Resources\NotificationSettingResource;
+use Filament\Resources\Pages\CreateRecord;
 
-    class CreateNotificationSetting extends CreateRecord
+class CreateNotificationSetting extends CreateRecord
+{
+    protected static string $resource = NotificationSettingResource::class;
+
+    protected static ?string $title = "Add New Global Notification";
+
+    protected function mutateFormDataBeforeCreate(array $data): array
     {
-        protected static string $resource = NotificationSettingResource::class;
-
-        protected static ?string $title = "Add New Notification Channel";
-
-        protected static ?string $breadcrumb = 'Add new';
-
-        protected function getRedirectUrl(): string
-        {
-            return $this->previousUrl ?? $this->getResource()::getUrl('index');
+        $data['user_id'] = auth()->id();
+        $data['scope'] = NotificationScopesEnum::GLOBAL->name;
+        
+        if ($data['channel_type'] === NotificationChannelTypesEnum::MAIL->name) {
+            $data['notification_channel_id'] = null;
         }
 
-        protected function mutateFormDataBeforeCreate( array $data ): array
-        {
-            $data[ 'user_id' ] = auth()->id();
-
-            if ( $data[ 'channel_type' ] === NotificationChannelTypesEnum::MAIL->name ) {
-                $data[ 'notification_channel_id' ] = null;
-            }
-
-            if ( $data[ 'channel_type' ] === NotificationChannelTypesEnum::WEBHOOK->name ) {
-                $data[ 'address' ] = null;
-            }
-
-            return $data;
+        if ($data['channel_type'] === NotificationChannelTypesEnum::WEBHOOK->name) {
+            $data['address'] = null;
         }
+
+        return $data;
     }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->previousUrl ?? $this->getResource()::getUrl('index');
+    }
+}
