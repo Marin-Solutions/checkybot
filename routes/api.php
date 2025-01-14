86eaffa7b07c9b7b25a8f4fd2b22371b0547d5c7
Marin Delija <marindelija@gmail.com>
2025-01-14 13:26:17 +0100

    ..

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Api\V1\ServerController;
 use App\Http\Controllers\ServerInformationHistoryController;
 use App\Http\Controllers\ServerLogFileHistoryController;
 
-Route::prefix('v1')->middleware(['api', 'api.key'])->group(function () {
+Route::prefix('v1')->middleware(['api'])->group(function () {
     Route::apiResource('servers', ServerController::class);
 
     Route::post('/server-history', [ServerInformationHistoryController::class, 'store']);
