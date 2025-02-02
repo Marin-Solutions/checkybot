0837ae45fbeef0b9964076bf752a33da35623846
Marin Delija <marindelija@gmail.com>
2025-02-02 21:53:55 +0100

    ..

--- app/Models/NotificationChannels.php
+++ app/Models/NotificationChannels.php
@@ -1,120 +1,119 @@
 <?php
 
-    namespace App\Models;
+namespace App\Models;
 
-    use App\Enums\WebhookHttpMethod;
-    use GuzzleHttp\Exception\RequestException;
-    use Illuminate\Database\Eloquent\Factories\HasFactory;
-    use Illuminate\Database\Eloquent\Model;
-    use Illuminate\Support\Facades\Http;
+use App\Enums\WebhookHttpMethod;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Database\Eloquent\Model;
+use GuzzleHttp\Exception\RequestException;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-    class NotificationChannels extends Model
+class NotificationChannels extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'method',
+        'url',
+        'description',
+        'request_body',
+        'created_by',
+    ];
+
+    protected $casts = [
+        'request_body' => 'array'
+    ];
+
+    public static function testWebhook(array $data): array
     {
-        use HasFactory;
+        $messageTest  = "Hello, I'm from " . url('/');
+        $method       = $data['method'];
+        $url          = $data['url'];
+        $responseData = [];
+        $requestBody  = @$data['request_body'] ?? [];
 
-        protected $fillable = [
-            'title',
-            'method',
-            'url',
-            'description',
-            'request_body',
-            'created_by',
-        ];
+        try {
+            if (str_contains($url, '{message}')) {
+                $url = str_replace('{message}', $messageTest, $url);
+            }
+            if (str_contains($url, '{description}')) {
+                $url = str_replace('{description}', $data['description'] ?? '', $url);
+            }
 
-        protected $casts = [
-            'request_body' => 'array'
-        ];
-
-        public static function testWebhook( array $data ): array
-        {
-            $messageTest  = "Hello, I'm from " . url('/');
-            $method       = $data[ 'method' ];
-            $url          = $data[ 'url' ];
-            $responseData = [];
-            $requestBody  = @$data[ 'request_body' ] ?? [];
-
-            try {
-                if ( str_contains($url, '{message}') ) {
-                    $url = str_replace('{message}', $messageTest, $url);
-                }
-                if ( str_contains($url, '{description}') ) {
-                    $url = str_replace('{description}', $data[ 'description' ] ?? '', $url);
-                }
-
-                if ( $method === WebhookHttpMethod::POST->value && count($data[ 'request_body' ]) ) {
-                    foreach ( $requestBody as $key => $value ) {
-                        if ( $value === '{message}' ) {
-                            $requestBody[ $key ] = $messageTest;
-                        }
-                        if ( $value === '{description}' ) {
-                            $requestBody[ $key ] = $data[ 'description' ] ?? '';
-                        }
+            if ($method === WebhookHttpMethod::POST->value && count($data['request_body'])) {
+                foreach ($requestBody as $key => $value) {
+                    if ($value === '{message}') {
+                        $requestBody[$key] = $messageTest;
+                    }
+                    if ($value === '{description}') {
+                        $requestBody[$key] = $data['description'] ?? '';
                     }
                 }
-
-                $webhookCallback = match ( $method ) {
-                    WebhookHttpMethod::GET->value => Http::{$method}($url),
-                    default => Http::{$method}($url, $requestBody)
-                };
-
-                $responseData[ 'code' ] = $webhookCallback->ok() ? 200 : 0;
-                $responseData[ 'body' ] = $webhookCallback->json();
-                return $responseData;
-
-            } catch ( RequestException $exception ) {
-
-                $handlerContext         = $exception->getHandlerContext();
-                $responseData[ 'code' ] = $handlerContext[ 'errno' ];
-                $responseData[ 'body' ] = $handlerContext[ 'error' ];
-                return $responseData;
-
             }
-        }
 
-        public function sendWebhookNotification( array $data ): array
-        {
-            $messageText     = @$data[ 'message' ] ?? "Hello, I'm from " . url('/');
-            $descriptionText = @$data[ 'description' ] ?? "Description Text";
-            $method          = $this->method;
-            $url             = $this->url;
-            $responseData    = [ 'url' => $url ];
-            $requestBody     = $this->request_body;
+            $webhookCallback = match ($method) {
+                WebhookHttpMethod::GET->value => Http::{$method}($url),
+                default => Http::{$method}($url, $requestBody)
+            };
 
-            try {
-                if ( str_contains($url, '{message}') ) {
-                    $url = str_replace('{message}', $messageText, $url);
-                }
-                if ( str_contains($url, '{description}') ) {
-                    $url = str_replace('{description}', $descriptionText, $url);
-                }
+            $responseData['code'] = $webhookCallback->ok() ? 200 : 0;
+            $responseData['body'] = $webhookCallback->json();
+            return $responseData;
+        } catch (RequestException $exception) {
 
-                if ( $method === WebhookHttpMethod::POST->value && count($requestBody) ) {
-                    foreach ( $requestBody as $key => $value ) {
-                        if ( $value === '{message}' ) {
-                            $requestBody[ $key ] = $messageText;
-                        }
-                        if ( $value === '{description}' ) {
-                            $requestBody[ $key ] = $descriptionText;
-                        }
-                    }
-                }
-
-                $webhookCallback = match ( $method ) {
-                    WebhookHttpMethod::GET->value => Http::{$method}($url),
-                    default => Http::{$method}($url, $requestBody)
-                };
-
-                $responseData[ 'code' ] = $webhookCallback->ok() ? 200 : 0;
-                $responseData[ 'body' ] = $webhookCallback->json();
-                return $responseData;
-
-            } catch ( RequestException $exception ) {
-
-                $handlerContext         = $exception->getHandlerContext();
-                $responseData[ 'code' ] = $handlerContext[ 'errno' ];
-                $responseData[ 'body' ] = $handlerContext[ 'error' ];
-                return $responseData;
-
-            }
+            $handlerContext         = $exception->getHandlerContext();
+            $responseData['code'] = $handlerContext['errno'];
+            $responseData['body'] = $handlerContext['error'];
+            return $responseData;
         }
     }
+
+    public function sendWebhookNotification(array $data): array
+    {
+        $messageText     = @$data['message'] ?? "Hello, I'm from " . url('/');
+        $descriptionText = @$data['description'] ?? "Description Text";
+        $method          = $this->method;
+        $url             = $this->url;
+        $responseData    = ['url' => $url];
+        $requestBody     = $this->request_body;
+
+        try {
+            if (str_contains($url, '{message}')) {
+                $url = str_replace('{message}', $messageText, $url);
+            }
+            if (str_contains($url, '{description}')) {
+                $url = str_replace('{description}', $descriptionText, $url);
+            }
+
+            if ($method === WebhookHttpMethod::POST->value && count($requestBody)) {
+                foreach ($requestBody as $key => $value) {
+                    if ($value === '{message}') {
+                        $requestBody[$key] = $messageText;
+                    }
+                    if ($value === '{description}') {
+                        $requestBody[$key] = $descriptionText;
+                    }
+                }
+            }
+
+            $webhookCallback = match ($method) {
+                WebhookHttpMethod::GET->value => Http::{$method}($url),
+                default => Http::{$method}($url, $requestBody)
+            };
+
+            $responseData['code'] = $webhookCallback->ok() ? 200 : 0;
+            $responseData['body'] = $webhookCallback->json();
+            return $responseData;
+        } catch (RequestException $exception) {
+
+            Log::error($exception->getMessage());
+
+            $handlerContext         = $exception->getHandlerContext();
+            $responseData['code'] = $handlerContext['errno'];
+            $responseData['body'] = $handlerContext['error'];
+            return $responseData;
+        }
+    }
+}
