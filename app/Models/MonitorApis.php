5c8470e428e68e38274abfd81638c51011a2604c
Marin Delija <marindelija@gmail.com>
2025-02-02 21:26:59 +0100

    Add user relationship to MonitorApis model

--- app/Models/MonitorApis.php
+++ app/Models/MonitorApis.php
@@ -6,6 +6,7 @@ use GuzzleHttp\Exception\RequestException;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Http;
 
@@ -19,6 +20,11 @@ class MonitorApis extends Model
         'data_path',
         'created_by'
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
 
     public function assertions(): HasMany
     {
