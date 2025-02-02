40dd59e09efa0ceb3f63345fb7572a7b968bb2f8
Marin Delija <marindelija@gmail.com>
2025-02-02 20:31:00 +0100

    ..

--- app/Filament/Resources/MonitorApisResource/Pages/EditMonitorApis.php
+++ app/Filament/Resources/MonitorApisResource/Pages/EditMonitorApis.php
@@ -1,39 +1,46 @@
 <?php
 
-    namespace App\Filament\Resources\MonitorApisResource\Pages;
+namespace App\Filament\Resources\MonitorApisResource\Pages;
 
-    use App\Filament\Resources\MonitorApisResource;
-    use App\Traits\MonitoringApis;
-    use Filament\Actions;
-    use Filament\Resources\Pages\EditRecord;
+use App\Filament\Resources\MonitorApisResource;
+use App\Traits\MonitoringApis;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
 
-    class EditMonitorApis extends EditRecord
+class EditMonitorApis extends EditRecord
+{
+    use MonitoringApis;
+
+    protected static string $resource = MonitorApisResource::class;
+
+    protected function getRedirectUrl(): string
     {
-        use MonitoringApis;
-
-        protected static string $resource = MonitorApisResource::class;
-
-        protected function getRedirectUrl(): string
-        {
-            return $this->previousUrl ?? $this->getResource()::getUrl('index');
-        }
-
-        protected function getHeaderActions(): array
-        {
-            return [
-                Actions\DeleteAction::make(),
-            ];
-        }
-
-        public function doMonitoring(): void
-        {
-            $this->callDoMonitoring($this->form);
-        }
-
-        protected function getFormActions(): array
-        {
-            return array_merge([
-                $this->doMonitorApiAction()
-            ], parent::getFormActions());
-        }
+        return $this->previousUrl ?? $this->getResource()::getUrl('index');
     }
+    
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            \App\Filament\Resources\MonitorApisResource\Widgets\ResponseTimeChart::make(['record' => $this->record]),
+        ];
+    }
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+
+    public function doMonitoring(): void
+    {
+        $this->callDoMonitoring($this->form);
+    }
+
+    protected function getFormActions(): array
+    {
+        return array_merge([
+            $this->doMonitorApiAction()
+        ], parent::getFormActions());
+    }
+}
