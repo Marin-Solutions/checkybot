f035bc29bbef5f11159ce0e32e86839ae3841436
Marin Delija <marindelija@gmail.com>
2025-02-03 10:43:26 +0100

    I apolog

---
+++ database/migrations/2025_02_04_000000_alter_response_body_column_in_monitor_api_results_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterResponseBodyColumnInMonitorApiResultsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('monitor_api_results', function (Blueprint $table) {
+            $table->longText('response_body')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('monitor_api_results', function (Blueprint $table) {
+            $table->text('response_body')->change();
+        });
+    }
+}
