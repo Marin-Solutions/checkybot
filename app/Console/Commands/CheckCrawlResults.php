e8a471e3ff5ed98feb77b1d99efb28e5dde1edea
John Paul Montilla <johnpaul.montilla1330@gmail.com>
2025-10-09 02:50:35 +0800

    removed tests

--- app/Console/Commands/CheckCrawlResults.php
+++ app/Console/Commands/CheckCrawlResults.php
@@ -64,4 +64,3 @@ class CheckCrawlResults extends Command
         }
     }
 }
-
--- app/Console/Commands/StartNewSeoCheck.php
+++ app/Console/Commands/StartNewSeoCheck.php
@@ -36,4 +36,3 @@ class StartNewSeoCheck extends Command
         $this->info("SEO check job dispatched for {$url}");
     }
 }
-
